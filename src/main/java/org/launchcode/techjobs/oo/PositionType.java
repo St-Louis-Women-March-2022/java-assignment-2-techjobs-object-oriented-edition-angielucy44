43372b3c556f391c949356b6fc2d64ba2cfa0b2d
Angie Zarinelli <angie.zarinelli@gmail.com>
2022-09-13 19:43:36 -0500

    completed Job class

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -22,27 +22,36 @@ public class PositionType {
 
     @Override
     public String toString() {
-        return "PositionType{" +
-                "value='" + value + '\'' +
-                '}';
+        return value;
     }
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PositionType)) return false;
-        PositionType that = (PositionType) o;
-        return getId() == that.getId() && Objects.equals(getValue(), that.getValue());
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof PositionType)) return false;
+//        PositionType that = (PositionType) o;
+//        return getId() == that.getId() && Objects.equals(getValue(), that.getValue());
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId(), getValue());
+//    }
+@Override
+public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+    if (this == o) return true;
+    if (!(o instanceof PositionType)) return false;
+    PositionType positionType = (PositionType) o;
+    return getId() == positionType.getId();
+}
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getValue());
+        return Objects.hash(getId());
     }
-
 
     // Getters and Setters:
 
